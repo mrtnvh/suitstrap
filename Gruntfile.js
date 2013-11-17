1e02b9c62c474f5322fff12df4589b6aa9ebd25a
Maarten Van Hoof <maarten@maartenvanhoof.be>
2013-11-17 22:02:07 +0100

    Updated: Components and utilities to new naming convention Updated: package.json to include grunt-contrib-compass, changed package name and version numbering to Suitstrap’s versioning. Updated: Gruntfile.js to compile Compass
    Added: Bundler.io Gemfile
    Added: Compass’ config.rb
    Added: suitstrap.css in dist/css, trying to successfully compile suitstrap.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,222 +1,239 @@
 /* jshint node: true */
 
 module.exports = function(grunt) {
-  "use strict";
+	"use strict";
 
-  var btoa = require('btoa')
-  // Project configuration.
-  grunt.initConfig({
+	var btoa = require('btoa')
 
-    // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*!\n' +
-              ' * Bootstrap v<%= pkg.version %> by @fat and @mdo\n' +
-              ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-              ' * Licensed under <%= _.pluck(pkg.licenses, "url").join(", ") %>\n' +
-              ' *\n' +
-              ' * Designed and built with all the love in the world by @mdo and @fat.\n' +
-              ' */\n',
-    jqueryCheck: 'if (typeof jQuery === "undefined") { throw new Error("Bootstrap requires jQuery") }\n\n',
+	/* Project configuration. */
+	grunt.initConfig({
 
-    // Task configuration.
-    clean: {
-      dist: ['dist']
-    },
+	/* ==========================================================================
+		Metadata
+		========================================================================== */
+		pkg: grunt.file.readJSON('package.json'),
+		banner: '/*!\n' +
+							' * Suitstrap <%= pkg.version %> by @vanhoofmaarten\n' +
+							' *\n' +
+							' * Forked from Bootstrap v3.0.0 by @fat and @mdo\n' +
+							' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+							' * Licensed under <%= _.pluck(pkg.licenses, "url").join(", ") %>\n' +
+							' *\n' +
+							' * Designed and built with all the love in the world by @mdo and @fat.\n' +
+							' */\n',
+		jqueryCheck: 'if (typeof jQuery === "undefined") { throw new Error("Bootstrap requires jQuery") }\n\n',
 
-    jshint: {
-      options: {
-        jshintrc: 'js/.jshintrc'
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      src: {
-        src: ['js/*.js']
-      },
-      test: {
-        src: ['js/tests/unit/*.js']
-      }
-    },
+	/* ==========================================================================
+		Clean
+		========================================================================== */
+		clean: {
+			dist: ['dist']
+		},
 
-    concat: {
-      options: {
-        banner: '<%= banner %><%= jqueryCheck %>',
-        stripBanners: false
-      },
-      bootstrap: {
-        src: [
-          'js/transition.js',
-          'js/alert.js',
-          'js/button.js',
-          'js/carousel.js',
-          'js/collapse.js',
-          'js/dropdown.js',
-          'js/modal.js',
-          'js/tooltip.js',
-          'js/popover.js',
-          'js/scrollspy.js',
-          'js/tab.js',
-          'js/affix.js'
-        ],
-        dest: 'dist/js/<%= pkg.name %>.js'
-      }
-    },
+	/* ==========================================================================
+		JSHint
+		========================================================================== */
+		jshint: {
+			options: {
+				jshintrc: 'js/.jshintrc'
+			},
+			gruntfile: {
+				src: 'Gruntfile.js'
+			},
+			src: {
+				src: ['js/*.js']
+			},
+			test: {
+				src: ['js/tests/unit/*.js']
+			}
+		},
 
-    uglify: {
-      options: {
-        banner: '<%= banner %>',
-        report: 'min'
-      },
-      bootstrap: {
-        src: ['<%= concat.bootstrap.dest %>'],
-        dest: 'dist/js/<%= pkg.name %>.min.js'
-      }
-    },
+	/* ==========================================================================
+		Concat
+		========================================================================== */
+		concat: {
+			options: {
+				banner: '<%= banner %><%= jqueryCheck %>',
+				stripBanners: false
+			},
+			bootstrap: {
+				src: [
+					'js/transition.js',
+					'js/alert.js',
+					'js/button.js',
+					'js/carousel.js',
+					'js/collapse.js',
+					'js/dropdown.js',
+					'js/modal.js',
+					'js/tooltip.js',
+					'js/popover.js',
+					'js/scrollspy.js',
+					'js/tab.js',
+					'js/affix.js'
+				],
+				dest: 'dist/js/<%= pkg.name %>.js'
+			}
+		},
 
-    recess: {
-      options: {
-        compile: true,
-        banner: '<%= banner %>'
-      },
-      bootstrap: {
-        src: ['less/bootstrap.less'],
-        dest: 'dist/css/<%= pkg.name %>.css'
-      },
-      min: {
-        options: {
-          compress: true
-        },
-        src: ['less/bootstrap.less'],
-        dest: 'dist/css/<%= pkg.name %>.min.css'
-      },
-      theme: {
-        src: ['less/theme.less'],
-        dest: 'dist/css/<%= pkg.name %>-theme.css'
-      },
-      theme_min: {
-        options: {
-          compress: true
-        },
-        src: ['less/theme.less'],
-        dest: 'dist/css/<%= pkg.name %>-theme.min.css'
-      }
-    },
+	/* ==========================================================================
+		Uglify
+		========================================================================== */
+		uglify: {
+			options: {
+				banner: '<%= banner %>',
+				report: 'min'
+			},
+			bootstrap: {
+				src: ['<%= concat.bootstrap.dest %>'],
+				dest: 'dist/js/<%= pkg.name %>.min.js'
+			}
+		},
 
-    copy: {
-      fonts: {
-        expand: true,
-        src: ["fonts/*"],
-        dest: 'dist/'
-      }
-    },
+	/* ==========================================================================
+		Compass
+		========================================================================== */
+		compass: {
+			development :{
+				options: {
+						config: 'config.rb'
+				}
+			}
+		},
 
-    qunit: {
-      options: {
-        inject: 'js/tests/unit/phantom.js'
-      },
-      files: ['js/tests/*.html']
-    },
+	/* ==========================================================================
+		Copy
+		========================================================================== */
+		copy: {
+			fonts: {
+				expand: true,
+				src: ["fonts/*"],
+				dest: 'dist/'
+			}
+		},
 
-    connect: {
-      server: {
-        options: {
-          port: 3000,
-          base: '.'
-        }
-      }
-    },
+	/* ==========================================================================
+		Qunit
+		========================================================================== */
+		qunit: {
+			options: {
+				inject: 'js/tests/unit/phantom.js'
+			},
+			files: ['js/tests/*.html']
+		},
 
-    jekyll: {
-      docs: {}
-    },
+	/* ==========================================================================
+		Connect
+		========================================================================== */
+		connect: {
+			server: {
+				options: {
+					port: 3000,
+					base: '.'
+				}
+			}
+		},
 
-    validation: {
-      options: {
-        reset: true
-      },
-      files: {
-        src: ["_gh_pages/**/*.html"]
-      }
-    },
+	/* ==========================================================================
+		Jekyll
+		========================================================================== */
+		jekyll: {
+			docs: {}
+		},
 
-    watch: {
-      src: {
-        files: '<%= jshint.src.src %>',
-        tasks: ['jshint:src', 'qunit']
-      },
-      test: {
-        files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'qunit']
-      },
-      recess: {
-        files: 'less/*.less',
-        tasks: ['recess']
-      }
-    }
-  });
+	/* ==========================================================================
+		Validation
+		========================================================================== */
+		validation: {
+			options: {
+				reset: true
+			},
+			files: {
+				src: ["_gh_pages/**/*.html"]
+			}
+		},
+
+	/* ==========================================================================
+		Watch
+		========================================================================== */
+		watch: {
+			src: {
+				files: '<%= jshint.src.src %>',
+				tasks: ['jshint:src', 'qunit']
+			},
+			test: {
+				files: '<%= jshint.test.src %>',
+				tasks: ['jshint:test', 'qunit']
+			},
+			compass:{
+				files: ["sass/**/*.scss"],
+				tasks: ['compass:development']
+			},
+		}
+	});
 
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('browserstack-runner');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-html-validation');
-  grunt.loadNpmTasks('grunt-jekyll');
-  grunt.loadNpmTasks('grunt-recess');
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('browserstack-runner');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-qunit');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-html-validation');
+	grunt.loadNpmTasks('grunt-jekyll');
+	grunt.loadNpmTasks('grunt-recess');
 
-  // Docs HTML validation task
-  grunt.registerTask('validate-html', ['jekyll', 'validation']);
+	// Docs HTML validation task
+	grunt.registerTask('validate-html', ['jekyll', 'validation']);
 
-  // Test task.
-  var testSubtasks = ['dist-css', 'jshint', 'qunit', 'validate-html'];
-  // Only run BrowserStack tests under Travis
-  if (process.env.TRAVIS) {
-    // Only run BrowserStack tests if this is a mainline commit in twbs/bootstrap, or you have your own BrowserStack key
-    if ((process.env.TRAVIS_REPO_SLUG === 'twbs/bootstrap' && process.env.TRAVIS_PULL_REQUEST === 'false') || process.env.TWBS_HAVE_OWN_BROWSERSTACK_KEY) {
-      testSubtasks.push('browserstack_runner');
-    }
-  }
-  grunt.registerTask('test', testSubtasks);
+	// Test task.
+	var testSubtasks = ['dist-css', 'jshint', 'qunit', 'validate-html'];
+	// Only run BrowserStack tests under Travis
+	if (process.env.TRAVIS) {
+		// Only run BrowserStack tests if this is a mainline commit in twbs/bootstrap, or you have your own BrowserStack key
+		if ((process.env.TRAVIS_REPO_SLUG === 'twbs/bootstrap' && process.env.TRAVIS_PULL_REQUEST === 'false') || process.env.TWBS_HAVE_OWN_BROWSERSTACK_KEY) {
+			testSubtasks.push('browserstack_runner');
+		}
+	}
+	grunt.registerTask('test', testSubtasks);
 
-  // JS distribution task.
-  grunt.registerTask('dist-js', ['concat', 'uglify']);
+	// JS distribution task.
+	grunt.registerTask('dist-js', ['concat', 'uglify']);
 
-  // CSS distribution task.
-  grunt.registerTask('dist-css', ['recess']);
+	// CSS distribution task.
+	grunt.registerTask('dist-css', ['recess']);
 
-  // Fonts distribution task.
-  grunt.registerTask('dist-fonts', ['copy']);
+	// Fonts distribution task.
+	grunt.registerTask('dist-fonts', ['copy']);
 
-  // Full distribution task.
-  grunt.registerTask('dist', ['clean', 'dist-css', 'dist-fonts', 'dist-js']);
+	// Full distribution task.
+	grunt.registerTask('dist', ['clean', 'dist-css', 'dist-fonts', 'dist-js']);
 
-  // Default task.
-  grunt.registerTask('default', ['test', 'dist', 'build-customizer']);
+	// Default task.
+	grunt.registerTask('default', ['test', 'dist', 'build-customizer']);
 
-  // task for building customizer
-  grunt.registerTask('build-customizer', 'Add scripts/less files to customizer.', function () {
-    var fs = require('fs')
+	// task for building customizer
+	grunt.registerTask('build-customizer', 'Add scripts/less files to customizer.', function () {
+		var fs = require('fs')
 
-    function getFiles(type) {
-      var files = {}
-      fs.readdirSync(type)
-        .filter(function (path) {
-          return type == 'fonts' ? true : new RegExp('\\.' + type + '$').test(path)
-        })
-        .forEach(function (path) {
-          var fullPath = type + '/' + path
-          return files[path] = (type == 'fonts' ? btoa(fs.readFileSync(fullPath)) : fs.readFileSync(fullPath, 'utf8'))
-        })
-      return 'var __' + type + ' = ' + JSON.stringify(files) + '\n'
-    }
+		function getFiles(type) {
+			var files = {}
+			fs.readdirSync(type)
+				.filter(function (path) {
+					return type == 'fonts' ? true : new RegExp('\\.' + type + '$').test(path)
+				})
+				.forEach(function (path) {
+					var fullPath = type + '/' + path
+					return files[path] = (type == 'fonts' ? btoa(fs.readFileSync(fullPath)) : fs.readFileSync(fullPath, 'utf8'))
+				})
+			return 'var __' + type + ' = ' + JSON.stringify(files) + '\n'
+		}
 
-    var files = getFiles('js') + getFiles('less') + getFiles('fonts')
-    fs.writeFileSync('docs-assets/js/raw-files.js', files)
-  });
+		var files = getFiles('js') + getFiles('less') + getFiles('fonts')
+		fs.writeFileSync('docs-assets/js/raw-files.js', files)
+	});
 };
